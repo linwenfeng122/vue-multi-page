bae21170924d8829e6b62e231e30de8107321c23
linwenfeng122 <812174597@qq.com>
2019-05-27 15:06:35 +0800

    vue-mulit-page first

---
+++ src/views/about/home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="home">
+    honme-index
+    <router-link to="/test">test</router-link>
+  </div>
+</template>
+
+<style>
+ 
+</style>
---
+++ src/views/index/home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="home">
+    honme-index
+     <a href="about.html">跳转新的页面</a>
+  </div>
+</template>
+
+<style>
+ 
+</style>
